ec92e384794f9fa732a9875e2db5307d7e9b92c0
Fırat Özden <fir4tozden@gmail.com>
2025-09-24 16:03:51 +0300

    v1.1.12

--- tests/DuplicateRules.test.ts
+++ tests/DuplicateRules.test.ts
@@ -1,9 +1,9 @@
 import { javascript_rules, typescript_rules } from '../src/defaults/NeatlintOptions.default';
 
+const ignored_rules = ['@typescript-eslint/consistent-type-definitions'];
+
 const config_rules_keys = Object.keys({ ...(javascript_rules.configs as object), ...(typescript_rules.configs as object) });
 const custom_rules_keys = Object.keys({ ...(javascript_rules.custom as object), ...(typescript_rules.custom as object) });
-
-const ignored_rules = ['@typescript-eslint/consistent-type-definitions'];
 
 const common_rules = config_rules_keys.filter((rule) => custom_rules_keys.includes(rule) && !ignored_rules.includes(rule));
 

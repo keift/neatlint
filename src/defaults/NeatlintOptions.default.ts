6168e7f605aee10fe89e01be72e41594741bddda
Fırat Özden <fir4tozden@gmail.com>
2025-09-24 16:03:23 +0300

    v1.1.12

--- src/defaults/NeatlintOptions.default.ts
+++ src/defaults/NeatlintOptions.default.ts
@@ -4,6 +4,67 @@ import ESLintJS from '@eslint/js';
 import type { ESLintConfig } from '../types/ESLintConfig.type';
 import type { NeatlintOptions } from '../types/NeatlintOptions.type';
 
+export const javascript_rules: { configs: ESLintConfig['rules']; custom: ESLintConfig['rules'] } = {
+  configs: {
+    ...ESLintJS.configs.recommended.rules,
+    ...(Object.assign(
+      {},
+      ...TSESLint.configs.strictTypeChecked.map((item) =>
+        Object.fromEntries(
+          Object.entries(item.rules ?? {})
+            .filter(([key]) => !key.startsWith('@typescript-eslint'))
+            .map(([key]) => [key, 'error'])
+        )
+      )
+    ) as ESLintConfig['rules']),
+    ...(Object.assign(
+      {},
+      ...TSESLint.configs.stylisticTypeChecked.map((item) =>
+        Object.fromEntries(
+          Object.entries(item.rules ?? {})
+            .filter(([key]) => !key.startsWith('@typescript-eslint'))
+            .map(([key]) => [key, 'error'])
+        )
+      )
+    ) as ESLintConfig['rules']),
+    ...TSESLint.configs.eslintRecommended.rules
+  },
+
+  custom: {
+    'func-style': ['error', 'expression', { allowArrowFunctions: true }],
+    'no-duplicate-imports': 'error',
+    'no-eval': 'error',
+    'no-new-func': 'error',
+    'no-new-wrappers': 'error',
+    'no-object-constructor': 'error',
+    'no-restricted-syntax': ['error', "CallExpression[callee.name='String']", "CallExpression[callee.name='Boolean']", "CallExpression[callee.name='parseFloat']", "CallExpression[callee.property.name='forEach']", 'DebuggerStatement', 'EmptyStatement', 'ForInStatement', 'LabeledStatement', 'SequenceExpression', 'SwitchStatement', 'WithStatement'],
+    'no-useless-call': 'error',
+    'no-useless-computed-key': 'error',
+    'no-useless-concat': 'error',
+    'no-useless-rename': 'error',
+    'no-useless-return': 'error',
+    'object-shorthand': 'error',
+    'prefer-arrow-callback': 'error',
+    'prefer-template': 'error',
+    eqeqeq: 'error'
+  }
+};
+
+export const typescript_rules: { configs: ESLintConfig['rules']; custom: ESLintConfig['rules'] } = {
+  configs: {
+    ...(Object.assign({}, ...TSESLint.configs.strictTypeChecked.map((item) => item.rules ?? {})) as ESLintConfig['rules']),
+    ...(Object.assign({}, ...TSESLint.configs.stylisticTypeChecked.map((item) => item.rules ?? {})) as ESLintConfig['rules'])
+  },
+
+  custom: {
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-type-exports': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    '@typescript-eslint/prefer-readonly': 'error'
+  }
+};
+
 export const NeatlintOptionsDefault: NeatlintOptions = {
   disabled: false,
 
@@ -15,46 +76,8 @@ export const NeatlintOptionsDefault: NeatlintOptions = {
     plugins: {},
 
     rules: {
-      ...ESLintJS.configs.recommended.rules,
-      ...(Object.assign(
-        {},
-        ...TSESLint.configs.strictTypeChecked.map((item) =>
-          Object.fromEntries(
-            Object.entries(item.rules ?? {})
-              .filter(([key]) => !key.startsWith('@typescript-eslint'))
-              .map(([key]) => [key, 'error'])
-          )
-        )
-      ) as ESLintConfig['rules']),
-      ...(Object.assign(
-        {},
-        ...TSESLint.configs.stylisticTypeChecked.map((item) =>
-          Object.fromEntries(
-            Object.entries(item.rules ?? {})
-              .filter(([key]) => !key.startsWith('@typescript-eslint'))
-              .map(([key]) => [key, 'error'])
-          )
-        )
-      ) as ESLintConfig['rules']),
-      ...TSESLint.configs.eslintRecommended.rules,
-      'no-undef': 'off',
-
-      'func-style': ['error', 'expression', { allowArrowFunctions: true }],
-      'no-duplicate-imports': 'error',
-      'no-eval': 'error',
-      'no-new-func': 'error',
-      'no-new-wrappers': 'error',
-      'no-object-constructor': 'error',
-      'no-restricted-syntax': ['error', "CallExpression[callee.name='String']", "CallExpression[callee.name='Boolean']", "CallExpression[callee.name='parseFloat']", "CallExpression[callee.property.name='forEach']", 'DebuggerStatement', 'EmptyStatement', 'ForInStatement', 'LabeledStatement', 'SequenceExpression', 'SwitchStatement', 'WithStatement'],
-      'no-useless-call': 'error',
-      'no-useless-computed-key': 'error',
-      'no-useless-concat': 'error',
-      'no-useless-rename': 'error',
-      'no-useless-return': 'error',
-      'object-shorthand': 'error',
-      'prefer-arrow-callback': 'error',
-      'prefer-template': 'error',
-      eqeqeq: 'error'
+      ...javascript_rules.configs,
+      ...javascript_rules.custom
     }
   },
 
@@ -72,14 +95,8 @@ export const NeatlintOptionsDefault: NeatlintOptions = {
     },
 
     rules: {
-      ...(Object.assign({}, ...TSESLint.configs.strictTypeChecked.map((item) => item.rules ?? {})) as ESLintConfig['rules']),
-      ...(Object.assign({}, ...TSESLint.configs.stylisticTypeChecked.map((item) => item.rules ?? {})) as ESLintConfig['rules']),
-
-      '@typescript-eslint/consistent-type-definitions': ['error', 'type'], // stylisticTypeChecked but different
-      '@typescript-eslint/consistent-type-exports': 'error',
-      '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/explicit-member-accessibility': 'error',
-      '@typescript-eslint/prefer-readonly': 'error'
+      ...typescript_rules.configs,
+      ...typescript_rules.custom
     }
   }
 };
---
+++ tests/DuplicateRules.test.ts
@@ -0,0 +1,10 @@
+import { javascript_rules, typescript_rules } from '../src/defaults/NeatlintOptions.default';
+
+const config_rules_keys = Object.keys({ ...(javascript_rules.configs as object), ...(typescript_rules.configs as object) });
+const custom_rules_keys = Object.keys({ ...(javascript_rules.custom as object), ...(typescript_rules.custom as object) });
+
+const ignored_rules = ['@typescript-eslint/consistent-type-definitions'];
+
+const common_rules = config_rules_keys.filter((rule) => custom_rules_keys.includes(rule) && !ignored_rules.includes(rule));
+
+if (common_rules.length !== 0) throw Error(`Duplicate rules found: ${common_rules.join(', ')}`);

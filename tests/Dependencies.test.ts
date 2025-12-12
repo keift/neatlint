a857e8bb4af74554dbd350a1f6d424529636d933
fir4tozden <fir4tozden@gmail.com>
2025-12-12 21:23:03 +0300

    v1.2.1

---
+++ tests/Dependencies.test.ts
@@ -0,0 +1,10 @@
+import Package from '../package.json';
+
+if (Package.dependencies['@eslint/js'] !== 'latest') throw new Error('❌ Error: @eslint/js');
+if (Package.dependencies['@types/node'] !== 'latest') throw new Error('❌ Error: @types/node');
+if (Package.dependencies.eslint !== 'latest') throw new Error('❌ Error: eslint');
+if (Package.dependencies.jiti !== 'latest') throw new Error('❌ Error: jiti');
+if (Package.dependencies.typescript !== 'latest') throw new Error('❌ Error: typescript');
+if (Package.dependencies['typescript-eslint'] !== 'latest') throw new Error('❌ Error: typescript-eslint');
+
+console.log('✅ Success');

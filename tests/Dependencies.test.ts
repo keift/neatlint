34a6ca717fc5ede486e7ef8dfacc6476a2fe0a37
fir4tozden <fir4tozden@gmail.com>
2025-12-16 23:41:36 +0300

    v1.2.2

--- tests/Dependencies.test.ts
+++ tests/Dependencies.test.ts
@@ -1,11 +1,9 @@
 import Package from '../package.json';
 
 if (Package.dependencies['@eslint/js'] !== 'latest') throw new Error('❌ Error: @eslint/js');
-if (Package.dependencies['@types/lodash.merge'] !== 'latest') throw new Error('❌ Error: @types/lodash.merge');
 if (Package.dependencies['@types/node'] !== 'latest') throw new Error('❌ Error: @types/node');
 if (Package.dependencies.eslint !== 'latest') throw new Error('❌ Error: eslint');
 if (Package.dependencies.jiti !== 'latest') throw new Error('❌ Error: jiti');
-if (Package.dependencies['lodash.merge'] !== 'latest') throw new Error('❌ Error: lodash.merge');
 if (Package.dependencies.typescript !== 'latest') throw new Error('❌ Error: typescript');
 if (Package.dependencies['typescript-eslint'] !== 'latest') throw new Error('❌ Error: typescript-eslint');
 

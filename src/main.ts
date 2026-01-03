a6d3318dd8968a796cfc51f93604d8633f4bf172
fir4tozden <fir4tozden@gmail.com>
2026-01-03 20:50:04 +0300

    v1.0.0

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
 export * from './utils/Tlint.util';
 
-export type * from './barrels/Types.barrell';
+export type * from './barrels/Types.barrel';
--- tests/DuplicateRules.test.ts
+++ tests/DuplicateRules.test.ts
@@ -1,4 +1,4 @@
-import { js_rules, ts_rules } from '../src/defaults/TlintOptions.defaultt';
+import { js_rules, ts_rules } from '../src/defaults/TlintOptions.default';
 
 const ignored_rules = ['@typescript-eslint/consistent-type-definitions'];
 
